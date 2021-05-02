9ef2cc2b1a44585a59f589a6c71719dd7c1f1763
kiyohken2000 <www.broadwell.com@gmail.com>
2021-05-02 12:05:26 +0900

    master

--- src/routes/navigation/Navigation.js
+++ src/routes/navigation/Navigation.js
@@ -52,7 +52,7 @@ export default function App() {
     });
   }, []);
 
-   (async () => {
+  (async () => {
     const { status: existingStatus } = await Permissions.getAsync(
       Permissions.NOTIFICATIONS
     );
