cc3a38d12f710e2eceefae8ab0fdba84a2089086
kiyohken2000 <www.broadwell.com@gmail.com>
2021-05-07 13:06:54 +0900

    master

--- src/scenes/home/Home.js
+++ src/scenes/home/Home.js
@@ -11,7 +11,7 @@ import Icon from 'react-native-vector-icons/Feather'
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
-    shouldPlaySound: false,
+    shouldPlaySound: true,
     shouldSetBadge: false,
   }),
 });
